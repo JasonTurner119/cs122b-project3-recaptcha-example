d71aaf17d35fa4e04f640116d799a38662a9f0be
JasonTurner <jasonturner119@icloud.com>
2024-10-29 20:03:36 -0700

    Modernized RecaptchaVerifyUtils.verify

--- src/RecaptchaVerifyUtils.java
+++ src/RecaptchaVerifyUtils.java
@@ -1,52 +1,39 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import javax.net.ssl.HttpsURLConnection;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
 
 public class RecaptchaVerifyUtils {
 
     public static final String SITE_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
 
-    public static RecaptchaVerificationStatus verify(String gRecaptchaResponse) throws IOException {
-        URL verifyUrl = new URL(SITE_VERIFY_URL);
+    private static final HttpClient httpClient = HttpClient.newHttpClient();
 
-        // Open Connection to URL
-        HttpsURLConnection conn = (HttpsURLConnection) verifyUrl.openConnection();
+    public static RecaptchaVerificationStatus verify(String gRecaptchaResponse) {
+        String parameters =
+            "secret=" + RecaptchaConstants.SECRET_KEY +
+            "&response=" + gRecaptchaResponse;
 
-        // Add Request Header
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
-        conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(SITE_VERIFY_URL))
+            .header("User-Agent", "Mozilla/5.0")
+            .header("Accept-Language", "en-US,en;q=0.5")
+            .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+            .POST(HttpRequest.BodyPublishers.ofString(parameters))
+            .build();
 
-        // Data will be sent to the server.
-        String postParams = "secret=" + RecaptchaConstants.SECRET_KEY + "&response=" + gRecaptchaResponse;
-
-        // Send Request
-        conn.setDoOutput(true);
-
-        // Get the output stream of Connection
-        // Write data in this stream, which means to send data to Server.
-        OutputStream outStream = conn.getOutputStream();
-        outStream.write(postParams.getBytes());
-
-        outStream.flush();
-        outStream.close();
-
-        // Get the InputStream from Connection to read data sent from the server.
-        InputStream inputStream = conn.getInputStream();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-
-        JsonObject jsonObject = new Gson().fromJson(inputStreamReader, JsonObject.class);
-
-        inputStreamReader.close();
-
-        if (jsonObject.get("success").getAsBoolean()) {
-            // verification succeed
-            return RecaptchaVerificationStatus.VERIFIED;
+        try {
+            HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            JsonObject jsonObject = new Gson().fromJson(httpResponse.body(), JsonObject.class);
+            boolean success = jsonObject.get("success").getAsBoolean();
+            if (success) {
+                return RecaptchaVerificationStatus.VERIFIED;
+            }
+        } catch (Exception exception) {
+            System.out.println("Error while verifying recaptcha response.");
+            System.out.println(exception.getMessage());
         }
 
         return RecaptchaVerificationStatus.UNVERIFIED;
