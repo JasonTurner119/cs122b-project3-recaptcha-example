4a243ffed84fb9618584fa1e4d7e03212403a1d1
JasonTurner <jasonturner119@icloud.com>
2024-10-31 16:27:43 -0700

    Created more helper functions.

--- src/FormRecaptcha.java
+++ src/FormRecaptcha.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.*;
 
 @WebServlet(name = "FormReCaptcha", urlPatterns = "/form-recaptcha")
@@ -12,22 +11,32 @@ public class FormRecaptcha extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
-    private static final String loginUser = "mytestuser";
-    private static final String loginPasswd = "My6$Password";
-    private static final String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
+    private static final String DATABASE_USER = "mytestuser";
+    private static final String DATABASE_PASSWORD = "My6$Password";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/moviedb";
     
     private static final String G_RECAPTCHA_RESPONSE_PARAMETER_NAME = "g-recaptcha-response";
     private static final String SEARCHED_STAR_NAME_PARAMETER_NAME = "name";
 
     private static final String GET_STARS_SQL = "SELECT * FROM stars WHERE name LIKE ?";
+    private static final int GET_STARS_SQL_STAR_NAME_INDEX = 1;
+    private static final String STAR_NAME_SQL_COLUMN_NAME = "stars.name";
+    private static final String STAR_ID_SQL_COLUMN_NAME = "stars.id";
+    
+    private static final String CONTENT_TYPE = "text/html";
     
     Connection databaseConnection;
     
     @Override
     public void init(ServletConfig config) {
         try {
-            databaseConnection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
-        } catch (SQLException exception) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseConnection = DriverManager.getConnection(
+                DATABASE_URL,
+                DATABASE_USER,
+                DATABASE_PASSWORD
+            );
+        } catch (SQLException | ClassNotFoundException exception) {
             System.out.println("Error connecting to database.");
             System.out.println(exception.getMessage());
         }
@@ -38,50 +47,58 @@ public class FormRecaptcha extends HttpServlet {
         HttpServletRequest request,
         HttpServletResponse response
     ) throws IOException {
-        
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
 
         String gRecaptchaResponse = request.getParameter(G_RECAPTCHA_RESPONSE_PARAMETER_NAME);
-        System.out.println("gRecaptchaResponse=" + gRecaptchaResponse);
 
         RecaptchaVerificationStatus verification = RecaptchaVerifyUtils.verify(gRecaptchaResponse);
         if (verification != RecaptchaVerificationStatus.VERIFIED) {
-            out.println(
-                getErrorHtml("Recaptcha Verification Error")
-            );
+            sendRecaptchaError(response);
             return;
         }
 
-        try (PreparedStatement statement = databaseConnection.prepareStatement(GET_STARS_SQL)) {
-
-            String name = request.getParameter(SEARCHED_STAR_NAME_PARAMETER_NAME);
-            statement.setString(1, name);
-
-            ResultSet resultSet = statement.executeQuery();
-
-            out.println(getLeadingHtml());
-
-            while (resultSet.next()) {
-                out.println(
-                    getStarTableRowHtml(resultSet)
-                );
-            }
-
-            out.println(getTrailingHtml());
-
-        } catch (Exception exception) {
-            out.println(
-                getErrorHtml(exception.getMessage())
-            );
-        }
+        String starName = request.getParameter(SEARCHED_STAR_NAME_PARAMETER_NAME);
+        performStarsQuery(starName, response);
         
+    }
+    
+    private void performStarsQuery(
+        String starName,
+        HttpServletResponse response
+    ) throws IOException {
+        try (PreparedStatement statement = databaseConnection.prepareStatement(GET_STARS_SQL)) {
+            statement.setString(GET_STARS_SQL_STAR_NAME_INDEX, starName);
+            ResultSet resultSet = statement.executeQuery();
+            sendStarsList(response, resultSet);
+        } catch (Exception exception) {
+            sendExceptionError(response, exception);
+        }
+    }
+    
+    private static void sendRecaptchaError(HttpServletResponse response) throws IOException {
+        response.setContentType(CONTENT_TYPE);
+        response.getWriter().println(getErrorHtml("Recaptcha Verification Error"));
+    }
+    
+    private static void sendExceptionError(
+        HttpServletResponse response,
+        Exception exception
+    ) throws IOException {
+        response.setContentType(CONTENT_TYPE);
+        response.getWriter().println(getErrorHtml(exception.getMessage()));
+    }
+    
+    private static void sendStarsList(
+        HttpServletResponse response,
+        ResultSet resultSet
+    ) throws IOException, SQLException {
+        response.setContentType(CONTENT_TYPE);
+        response.getWriter().println(getHtmlForStarsList(resultSet));
     }
 
     private static String getErrorHtml(String message) {
         return
             "<html>" +
-            "<head><title>Error</title></head>" +
+            "<head><title> Error </title></head>" +
             "<body>" +
             "<p>error: " + message + "</p>" +
             "</body>" +
@@ -89,21 +106,33 @@ public class FormRecaptcha extends HttpServlet {
         
     }
     
-    private static String getStarTableRowHtml(ResultSet resultSet) throws SQLException {
-        String starId = resultSet.getString("id");
-        String starName = resultSet.getString("name");
-        return String.format("<tr><td>%s</td><td>%s</td></tr>", starId, starName);
+    private static String getHtmlForStarsList(ResultSet resultSet) throws SQLException {
+        StringBuilder html = new StringBuilder();
+        html.append(getStarTableLeadingHtml());
+        while (resultSet.next()) {
+			html.append(getStarTableRowHtml(resultSet));
+        }
+        html.append(getStarTableTrailingHtml());
+        return html.toString();
     }
     
-    private static String getLeadingHtml() {
+    private static String getStarTableRowHtml(ResultSet resultSet) throws SQLException {
+        String starId = resultSet.getString(STAR_ID_SQL_COLUMN_NAME);
+        String starName = resultSet.getString(STAR_NAME_SQL_COLUMN_NAME);
+        return String.format("<tr><td> %s </td><td> %s </td></tr>", starId, starName);
+    }
+    
+    private static String getStarTableLeadingHtml() {
         return
-            "<html><head><title> MovieDB: Found Records </title></head>" +
-            "<body><h1> MovieDB: Found Records </h1>" +
+            "<html>" +
+            "<head><title> MovieDB: Found Records </title></head>" +
+            "<body>" +
+            "<h1> MovieDB: Found Records </h1>" +
             "<table border>" +
             "<tr><td> ID </td><td> Name </td></tr>";
     }
     
-    private static String getTrailingHtml() {
+    private static String getStarTableTrailingHtml() {
         return "</table></body></html>";
     }
     
--- src/RecaptchaVerifyUtils.java
+++ src/RecaptchaVerifyUtils.java
@@ -8,29 +8,15 @@ import java.net.URI;
 public class RecaptchaVerifyUtils {
 
     public static final String SITE_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
+    public static final String JSON_RESPONSE_SUCCESS_KEY = "success";
 
     private static final HttpClient httpClient = HttpClient.newHttpClient();
 
     public static RecaptchaVerificationStatus verify(String gRecaptchaResponse) {
-        String parameters =
-            "secret=" + RecaptchaConstants.SECRET_KEY +
-            "&response=" + gRecaptchaResponse;
-
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(SITE_VERIFY_URL))
-            .header("User-Agent", "Mozilla/5.0")
-            .header("Accept-Language", "en-US,en;q=0.5")
-            .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
-            .POST(HttpRequest.BodyPublishers.ofString(parameters))
-            .build();
-
+        HttpRequest request = buildRequest(gRecaptchaResponse);
         try {
-            HttpResponse<String> httpResponse = httpClient.send(
-                request,
-                HttpResponse.BodyHandlers.ofString()
-            );
-            JsonObject jsonObject = new Gson().fromJson(httpResponse.body(), JsonObject.class);
-            boolean success = jsonObject.get("success").getAsBoolean();
+            JsonObject jsonObject = performRequest(request);
+            boolean success = isValidResponse(jsonObject);
             if (success) {
                 return RecaptchaVerificationStatus.VERIFIED;
             }
@@ -38,8 +24,39 @@ public class RecaptchaVerifyUtils {
             System.out.println("Error while verifying recaptcha response.");
             System.out.println(exception.getMessage());
         }
-
         return RecaptchaVerificationStatus.UNVERIFIED;
+    }
+    
+    private static HttpRequest buildRequest(String gRecaptchaResponse) {
+        String parameters = String.format(
+            "secret=%s&response=%s",
+            RecaptchaConstants.SECRET_KEY,
+            gRecaptchaResponse
+        );
+        
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(SITE_VERIFY_URL))
+            .header("User-Agent", "Mozilla/5.0")
+            .header("Accept-Language", "en-US,en;q=0.5")
+            .header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+            .POST(HttpRequest.BodyPublishers.ofString(parameters))
+            .build();
+        
+        return request;
+    }
+    
+    private static JsonObject performRequest(HttpRequest request) throws Exception {
+        HttpResponse<String> httpResponse = httpClient.send(
+            request,
+            HttpResponse.BodyHandlers.ofString()
+        );
+        JsonObject jsonObject = new Gson().fromJson(httpResponse.body(), JsonObject.class);
+        return jsonObject;
+    }
+    
+    private static boolean isValidResponse(JsonObject jsonObject) {
+        return jsonObject.has(JSON_RESPONSE_SUCCESS_KEY)
+            && jsonObject.get(JSON_RESPONSE_SUCCESS_KEY).getAsBoolean();
     }
 
 }
